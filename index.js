824fdfec76c4c0abbea39c449093ce502aaf42dd
leiru <uriellopezaquino1.0@gmail.com>
2023-06-03 14:37:18 -0600

    Routing con Express.js

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const port = 300;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name : "Producto",
+    price : 2000
+  });
+});
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
