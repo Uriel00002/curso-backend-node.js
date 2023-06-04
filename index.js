dcd751146bf7547316887d43005d01bb1a9b11f8
leiru <uriellopezaquino1.0@gmail.com>
2023-06-03 23:23:55 -0600

    Middleware para HttpErrors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -16,6 +18,8 @@ app.get('/nueva-ruta', (req, res) => {
 
 routerApi(app);
 
+app.use(logErrors);
+app.use(errorHandler);
 
 // app.get('/users', (req, res) => {
 //   const { limit, offset } = req.query;
