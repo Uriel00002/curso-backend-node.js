2f92e04555b532c53e27afe0eb07121e0bd7b0b0
leiru <uriellopezaquino1.0@gmail.com>
2023-06-03 19:17:08 -0600

    Códigos de estado o HTTP response status codes

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -10,23 +10,5 @@ router.get('/:categoryId/products/:productId', (req, res) => {
   });
 });
 
-router.patch('/:id',(req,res)=>{
-  const{id}=req.params;
-  const body=req.body;
-  res.json(
-    {message:'update',
-    data:body,
-    id,
-  });
-});
-
-router.delete('/:id',(req,res)=>{
-  const{id}=req.params;
-  res.json({
-    message:'deleted',
-    id,
-  });
-});
-
 
 module.exports = router;
